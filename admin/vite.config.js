7fdb15cd12b5f741f524cea8f323ee440b25ad8f
Urvitsanandiya <161760540+Urvitsanandiya@users.noreply.github.com>
2025-03-20 23:27:47 +0530

    Fix: Vite config

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -9,4 +9,9 @@ export default defineConfig({
     port: 4173, // Use Render's port or default to 4173
     host: "0.0.0.0", // Bind to all network interfaces
   },
+  preview: {
+    port: 4173,
+    host: "0.0.0.0",
+    allowedHosts: ["e-commerse-1-25gc.onrender.com"], // Add your Render domain here
+  },
 });
