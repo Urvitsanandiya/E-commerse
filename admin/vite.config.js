c2fc42a54a6cf42009f5d145cdc43c0ef165c191
Urvitsanandiya <161760540+Urvitsanandiya@users.noreply.github.com>
2025-03-20 23:21:05 +0530

    Fix: Configure Vite to use Render's port

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 4173, // Use Render's port or default to 4173
+    host: "0.0.0.0", // Bind to all network interfaces
+  },
 });
